56b0dfd2d54d6d35d3395567c52bb0843a6e71aa
CSTVann <sotheavannchhorn168@gmail.com>
2026-06-28 00:20:47 +0700

    Update index.html and CSS files: Replace logo image formats, enhance header styles, and add new About and Feature sections to improve content presentation and visual appeal.

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,7 +116,6 @@
   --------------------------------------------------------------*/
   function stickyHeader() {
     var $window = $(window);
-    var lastScrollTop = 0;
     var $header = $('.cs_sticky_header');
     var headerHeight = $header.outerHeight() + 20;
 
@@ -124,20 +123,10 @@
       var windowTop = $window.scrollTop();
 
       if (windowTop >= headerHeight) {
-        $header.addClass('cs_gescout_sticky');
+        $header.addClass('cs_gescout_sticky cs_gescout_show');
       } else {
-        $header.removeClass('cs_gescout_sticky');
-        $header.removeClass('cs_gescout_show');
+        $header.removeClass('cs_gescout_sticky cs_gescout_show');
       }
-
-      if ($header.hasClass('cs_gescout_sticky')) {
-        if (windowTop < lastScrollTop) {
-          $header.addClass('cs_gescout_show');
-        } else {
-          $header.removeClass('cs_gescout_show');
-        }
-      }
-      lastScrollTop = windowTop;
     });
   }
 
@@ -250,43 +239,7 @@
   /*--------------------------------------------------------------
     6. Modal Video
   --------------------------------------------------------------*/
-  function modalVideo() {
-    if ($.exists('.cs_video_open')) {
-      $('body').append(`
-        <div class="cs_video_popup">
-          <div class="cs_video_popup-overlay"></div>
-          <div class="cs_video_popup-content">
-            <div class="cs_video_popup-layer"></div>
-            <div class="cs_video_popup-container">
-              <div class="cs_video_popup-align">
-                <div class="embed-responsive embed-responsive-16by9">
-                  <iframe class="embed-responsive-item" src="about:blank"></iframe>
-                </div>
-              </div>
-              <div class="cs_video_popup-close"></div>
-            </div>
-          </div>
-        </div>
-      `);
-      $(document).on('click', '.cs_video_open', function (e) {
-        e.preventDefault();
-        var video = $(this).attr('href');
-
-        $('.cs_video_popup-container iframe').attr('src', `${video}`);
-
-        $('.cs_video_popup').addClass('active');
-      });
-      $('.cs_video_popup-close, .cs_video_popup-layer').on(
-        'click',
-        function (e) {
-          $('.cs_video_popup').removeClass('active');
-          $('html').removeClass('overflow-hidden');
-          $('.cs_video_popup-container iframe').attr('src', 'about:blank');
-          e.preventDefault();
-        },
-      );
-    }
-  }
+  
 
   /*--------------------------------------------------------------
     7. Scroll Up
